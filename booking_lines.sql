95292e92cf83f945c99525e0a814467566f80d1e
taldaitz <48563569+taldaitz@users.noreply.github.com>
2024-03-11 15:05:00 +0100

    Create booking_lines.sql

---
+++ booking_lines.sql
@@ -0,0 +1,120 @@
+INSERT INTO `booking` (`customer_firstname`,`customer_lastname`,`customer_phone_number`,`customer_email`,`start`,`end`,`room_number`,`room_type`)
+VALUES
+  ("Iliana","Thornton","07 16 98 73 25","nunc.mauris.elit@outlook.ca","2023-06-22","2024-11-29",36,"Double"),
+  ("Keiko","Randolph","08 36 01 27 15","et.eros@icloud.couk","2024-11-17","2024-06-09",4,"Premium"),
+  ("Maxine","Mcpherson","05 58 19 48 42","facilisis.suspendisse@outlook.org","2025-01-21","2024-06-29",22,"Single"),
+  ("Yardley","Paul","03 36 57 39 18","dignissim.pharetra@icloud.couk","2024-07-26","2023-06-08",34,"Double"),
+  ("Cade","Newman","04 36 69 01 35","quis.massa@yahoo.net","2024-05-10","2023-08-12",38,"Double"),
+  ("Hiroko","Roberson","03 84 44 42 61","sem.elit@google.couk","2023-10-28","2024-08-23",10,"Double"),
+  ("Montana","Mullins","01 62 97 66 97","neque.morbi@outlook.org","2023-04-21","2023-08-13",30,"Premium"),
+  ("Jocelyn","White","06 34 26 75 87","aliquam.ornare@icloud.edu","2023-10-17","2024-09-05",49,"Premium"),
+  ("Fiona","Talley","06 93 59 43 35","amet.ultricies@aol.edu","2023-07-11","2024-03-22",25,"Single"),
+  ("Lacy","Reilly","03 08 52 73 17","sed@google.net","2023-11-15","2023-07-21",1,"Premium");
+INSERT INTO `booking` (`customer_firstname`,`customer_lastname`,`customer_phone_number`,`customer_email`,`start`,`end`,`room_number`,`room_type`)
+VALUES
+  ("Shaeleigh","Robles","07 72 25 37 46","nunc.sed@aol.net","2023-11-28","2024-10-15",36,"Double"),
+  ("Denton","Cochran","08 10 66 77 30","placerat.cras@aol.org","2025-01-27","2024-06-16",50,"Double"),
+  ("Kasimir","Maddox","04 70 63 39 42","vitae.aliquet.nec@outlook.ca","2023-06-03","2023-09-05",44,"Single"),
+  ("Melanie","Bradley","06 17 56 11 38","iaculis.aliquet@aol.org","2023-09-15","2023-09-08",17,"Premium"),
+  ("Stephen","Schultz","07 50 49 08 94","eu.eleifend@hotmail.ca","2024-10-23","2024-12-02",46,"Single"),
+  ("Charity","Mcbride","04 81 57 60 25","non.cursus.non@outlook.com","2024-07-04","2023-12-23",55,"Premium"),
+  ("Cecilia","Brennan","06 43 72 65 72","eros.non.enim@outlook.edu","2025-02-16","2024-01-25",9,"Single"),
+  ("Rinah","Long","09 33 94 56 84","nunc.ullamcorper@icloud.org","2024-11-25","2023-05-11",54,"Premium"),
+  ("Justine","Hyde","02 98 87 52 25","vitae.velit.egestas@icloud.ca","2024-07-29","2023-08-27",9,"Single"),
+  ("Myles","Garcia","01 04 85 33 79","parturient.montes.nascetur@hotmail.couk","2024-08-07","2023-12-12",27,"Single");
+INSERT INTO `booking` (`customer_firstname`,`customer_lastname`,`customer_phone_number`,`customer_email`,`start`,`end`,`room_number`,`room_type`)
+VALUES
+  ("Victor","Barnes","05 76 08 13 25","eget@outlook.edu","2023-12-21","2025-02-15",41,"Double"),
+  ("Steel","Cabrera","08 28 06 56 89","sed.pede@yahoo.net","2024-12-25","2025-01-24",39,"Single"),
+  ("Todd","Pierce","03 79 71 37 58","ut.dolor.dapibus@icloud.couk","2024-10-20","2023-10-09",29,"Double"),
+  ("Zeph","Arnold","06 95 04 87 55","suspendisse.ac.metus@protonmail.edu","2023-04-28","2024-04-18",35,"Premium"),
+  ("Coby","Baldwin","05 41 54 42 55","eget.ipsum.donec@aol.net","2024-12-06","2024-11-11",49,"Single"),
+  ("Rogan","Horton","06 76 48 88 89","duis.elementum.dui@hotmail.net","2024-12-27","2025-03-01",48,"Single"),
+  ("Stuart","Poole","02 83 73 04 89","aenean.eget.metus@hotmail.org","2023-06-11","2023-04-28",41,"Premium"),
+  ("Brielle","Kennedy","02 90 85 87 56","mauris.blandit.enim@yahoo.net","2023-10-23","2025-01-13",47,"Double"),
+  ("Kenneth","James","02 53 11 68 24","vel@outlook.ca","2024-11-17","2024-01-22",18,"Double"),
+  ("Zachary","Olson","05 74 75 75 17","est.nunc@icloud.couk","2023-04-17","2023-12-19",50,"Single");
+INSERT INTO `booking` (`customer_firstname`,`customer_lastname`,`customer_phone_number`,`customer_email`,`start`,`end`,`room_number`,`room_type`)
+VALUES
+  ("Nina","Peck","04 46 62 57 48","vivamus.non.lorem@aol.org","2023-12-09","2024-06-20",32,"Single"),
+  ("Seth","Glenn","02 45 78 74 54","lacinia.sed@aol.com","2023-04-25","2024-03-02",58,"Single"),
+  ("Colin","Travis","08 63 18 58 47","a.odio@google.ca","2024-11-19","2023-07-25",16,"Premium"),
+  ("Brooke","Pena","04 85 57 27 51","et@aol.couk","2023-06-02","2023-07-07",25,"Premium"),
+  ("Margaret","Allen","04 42 88 84 86","id@outlook.couk","2025-01-03","2024-12-23",15,"Single"),
+  ("Flavia","Weber","07 34 45 35 39","nisi.dictum.augue@hotmail.couk","2024-11-13","2023-09-05",38,"Double"),
+  ("Samuel","Blake","07 13 65 35 49","interdum.enim@icloud.org","2023-12-13","2024-01-08",57,"Premium"),
+  ("Francis","Whitfield","03 83 19 78 14","magna.suspendisse.tristique@hotmail.net","2025-01-19","2024-10-01",47,"Single"),
+  ("Mannix","Velasquez","04 00 88 88 82","proin.vel@google.org","2024-01-23","2024-07-09",54,"Premium"),
+  ("Julie","Curtis","02 48 51 44 42","ac.mattis.ornare@icloud.org","2024-02-28","2023-04-08",37,"Double");
+INSERT INTO `booking` (`customer_firstname`,`customer_lastname`,`customer_phone_number`,`customer_email`,`start`,`end`,`room_number`,`room_type`)
+VALUES
+  ("Buffy","Mayo","05 54 37 85 53","natoque@outlook.ca","2024-08-25","2024-05-18",17,"Double"),
+  ("Josiah","Velasquez","08 99 12 92 71","lectus.ante@icloud.couk","2024-10-31","2024-10-27",21,"Single"),
+  ("Ralph","Bennett","02 37 52 60 24","nulla.facilisis.suspendisse@icloud.ca","2025-01-25","2024-10-24",1,"Double"),
+  ("Nehru","Leon","03 41 68 88 49","a.malesuada@outlook.net","2023-03-20","2024-12-25",44,"Double"),
+  ("Hedwig","Dillon","02 35 64 41 28","lectus@hotmail.net","2024-09-07","2024-06-13",25,"Double"),
+  ("Lev","Mitchell","06 04 61 84 44","rutrum.magna@hotmail.edu","2024-09-08","2023-06-10",28,"Double"),
+  ("Grace","Wilkins","06 75 55 72 47","sem.molestie@icloud.net","2025-02-26","2023-07-01",31,"Double"),
+  ("Signe","Rasmussen","03 37 79 95 80","cras.pellentesque.sed@yahoo.net","2024-11-08","2023-12-06",42,"Single"),
+  ("Iona","Ryan","03 11 22 33 76","sodales.nisi@icloud.org","2023-10-16","2024-03-11",5,"Single"),
+  ("Cleo","Lyons","03 65 69 92 38","non.magna@outlook.org","2024-10-02","2025-02-16",40,"Double");
+INSERT INTO `booking` (`customer_firstname`,`customer_lastname`,`customer_phone_number`,`customer_email`,`start`,`end`,`room_number`,`room_type`)
+VALUES
+  ("Lane","Newton","01 19 28 95 47","magnis.dis@outlook.net","2023-06-09","2025-02-25",29,"Premium"),
+  ("Jonas","Pugh","03 55 85 49 66","risus.a@protonmail.com","2024-07-19","2023-12-24",39,"Single"),
+  ("Hiroko","Washington","03 36 14 77 69","semper@aol.org","2024-09-06","2025-03-08",58,"Premium"),
+  ("Ethan","Floyd","05 34 59 46 81","tempus.non@yahoo.edu","2024-10-02","2024-05-30",2,"Premium"),
+  ("Cruz","Rivera","07 88 11 92 82","volutpat.nunc@icloud.org","2025-03-11","2024-07-23",46,"Double"),
+  ("Judith","Gordon","07 36 03 33 32","ut.ipsum@yahoo.edu","2023-08-14","2024-07-15",33,"Premium"),
+  ("Jillian","Donaldson","02 10 36 88 35","risus.morbi.metus@icloud.couk","2023-10-23","2023-03-24",9,"Single"),
+  ("Jesse","Dickerson","01 14 36 72 67","fringilla.donec@google.net","2023-11-24","2024-02-07",53,"Single"),
+  ("Gannon","Mcmillan","03 13 82 72 46","nec@yahoo.net","2024-11-20","2024-11-16",26,"Double"),
+  ("Lacota","Mathis","03 33 66 01 74","in@outlook.com","2024-02-12","2023-08-13",47,"Single");
+INSERT INTO `booking` (`customer_firstname`,`customer_lastname`,`customer_phone_number`,`customer_email`,`start`,`end`,`room_number`,`room_type`)
+VALUES
+  ("Owen","Shepard","09 21 65 48 56","nulla.ante@google.net","2024-02-12","2024-04-04",54,"Premium"),
+  ("Jakeem","Mueller","06 64 50 50 47","neque.sed.dictum@hotmail.net","2023-11-26","2023-10-09",57,"Single"),
+  ("Ashely","Mcneil","05 22 25 44 06","nibh.vulputate@aol.net","2023-03-23","2024-05-26",29,"Premium"),
+  ("Xenos","Hutchinson","08 08 76 08 64","venenatis.a.magna@hotmail.edu","2024-07-14","2023-05-05",1,"Single"),
+  ("Jillian","Moody","09 19 14 79 62","interdum.enim@google.edu","2023-08-28","2023-09-10",13,"Single"),
+  ("Athena","Blackburn","04 38 31 62 15","fusce@yahoo.ca","2024-07-14","2025-03-03",6,"Double"),
+  ("Oliver","Peck","05 25 74 88 14","iaculis@icloud.couk","2023-11-13","2024-06-08",26,"Double"),
+  ("Palmer","Goff","06 34 58 48 14","dictum@google.org","2024-01-13","2025-03-08",17,"Premium"),
+  ("Zoe","Graves","04 33 03 66 41","pede.ac@hotmail.edu","2023-04-02","2024-04-18",18,"Single"),
+  ("Hanae","Arnold","07 87 71 73 19","aliquet.proin.velit@google.com","2024-07-18","2023-10-30",19,"Double");
+INSERT INTO `booking` (`customer_firstname`,`customer_lastname`,`customer_phone_number`,`customer_email`,`start`,`end`,`room_number`,`room_type`)
+VALUES
+  ("Scarlett","Dale","03 79 26 88 59","facilisis.facilisis@hotmail.net","2024-11-02","2024-08-19",20,"Double"),
+  ("Abigail","Cox","06 77 86 58 80","id@yahoo.org","2023-12-29","2024-01-24",26,"Double"),
+  ("Ila","Lane","06 35 81 45 11","urna.nunc@protonmail.org","2024-12-10","2024-09-04",43,"Premium"),
+  ("Summer","Mathis","08 14 47 41 28","in.at@hotmail.org","2025-02-27","2024-08-04",35,"Premium"),
+  ("Dale","Riley","05 58 16 73 25","molestie.dapibus@google.com","2023-08-20","2024-05-07",14,"Premium"),
+  ("Baxter","Jensen","02 26 09 55 64","ante.nunc.mauris@yahoo.com","2023-07-18","2023-04-06",19,"Double"),
+  ("Brenden","Floyd","07 70 28 34 15","auctor.vitae@icloud.org","2025-02-11","2024-05-01",11,"Premium"),
+  ("Ethan","Sutton","08 24 46 68 85","sed.dui@protonmail.com","2024-02-22","2024-12-15",29,"Single"),
+  ("Dexter","Boyle","05 65 98 13 72","lorem@icloud.com","2025-01-20","2024-05-16",17,"Premium"),
+  ("Francesca","Dotson","04 81 58 48 46","semper.tellus@outlook.ca","2024-03-30","2023-07-07",53,"Premium");
+INSERT INTO `booking` (`customer_firstname`,`customer_lastname`,`customer_phone_number`,`customer_email`,`start`,`end`,`room_number`,`room_type`)
+VALUES
+  ("Dai","Thompson","01 33 52 39 69","nonummy.ac.feugiat@hotmail.couk","2024-04-02","2023-11-12",37,"Premium"),
+  ("Megan","Wiggins","03 73 24 81 75","posuere.at.velit@google.edu","2023-12-01","2023-09-03",25,"Double"),
+  ("Maxine","Pearson","02 27 57 63 90","amet.consectetuer@hotmail.com","2023-09-28","2024-09-14",6,"Double"),
+  ("Hoyt","Nielsen","05 33 91 93 62","hendrerit.donec@outlook.couk","2024-08-15","2023-03-13",45,"Double"),
+  ("Quinn","Young","06 80 95 37 61","erat@icloud.net","2024-12-30","2023-05-24",45,"Single"),
+  ("Kelly","Finch","07 47 61 27 62","vitae.purus@aol.ca","2024-01-18","2023-12-18",15,"Double"),
+  ("Chancellor","Snider","09 08 07 52 52","tortor.integer.aliquam@aol.couk","2023-08-24","2024-09-20",33,"Single"),
+  ("Rogan","Larsen","05 45 93 35 29","faucibus.orci@icloud.couk","2023-12-10","2024-10-24",32,"Premium"),
+  ("Miriam","Franco","03 11 15 59 74","fringilla.mi@outlook.com","2025-03-07","2023-11-28",13,"Double"),
+  ("Aimee","Strickland","08 12 41 87 47","ac.mattis@google.ca","2023-05-17","2023-12-29",44,"Single");
+INSERT INTO `booking` (`customer_firstname`,`customer_lastname`,`customer_phone_number`,`customer_email`,`start`,`end`,`room_number`,`room_type`)
+VALUES
+  ("Sonia","Finley","09 38 23 57 71","fermentum.convallis.ligula@hotmail.couk","2025-02-07","2023-12-23",19,"Double"),
+  ("Iola","Mack","03 13 52 26 75","nulla@yahoo.org","2024-10-21","2023-07-22",42,"Premium"),
+  ("Piper","Barlow","06 45 34 21 87","magna.tellus@icloud.ca","2023-06-19","2024-03-09",30,"Premium"),
+  ("Felix","Valencia","06 15 41 73 62","ipsum.non.arcu@hotmail.couk","2023-03-27","2024-03-06",27,"Single"),
+  ("Alika","Mathis","03 82 42 59 13","mi.lorem.vehicula@yahoo.net","2023-10-16","2024-10-15",21,"Double"),
+  ("Elijah","Coffey","02 82 19 18 17","massa.integer@aol.org","2024-11-28","2023-08-21",47,"Premium"),
+  ("Jemima","Garner","04 72 14 76 82","orci@protonmail.ca","2024-08-01","2024-09-06",22,"Single"),
+  ("Hermione","Vega","01 59 82 43 31","dui.cras.pellentesque@outlook.couk","2024-08-27","2024-12-02",2,"Premium"),
+  ("Jeremy","Malone","03 40 43 36 70","nunc.ullamcorper@icloud.net","2025-03-07","2023-09-16",22,"Premium"),
+  ("Russell","Henderson","02 26 83 87 46","ante.dictum@aol.org","2024-05-28","2023-05-06",33,"Single");
